69683cc8858b18856feb49869580f3466d2f202a
Chesney Julian <chesney.julian724@gmail.com>
2023-07-19 20:09:00 -0400

    create route to delete category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -59,8 +59,18 @@ router.put('/:id', async (req, res) => {
   };
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deletedCategoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    return res.status(200).json({message: 'Category Deleted'});
+  } catch (err) {
+    return res.status(500).json(err);
+  };
 });
 
 module.exports = router;
