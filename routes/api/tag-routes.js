125bf5c868da60718bf16b0d8b5e0db9d52bfb6d
Chesney Julian <chesney.julian724@gmail.com>
2023-07-22 12:38:26 -0400

    add route to get single tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -15,9 +15,19 @@ router.get('/', async (req, res) => {
   };
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id` be sure to include its associated Product data
+  try {
+    const singleTagData = await Tag.findByPk(req.params.id, {
+      include: [{model: Product}]
+    });
+    if (!singleTagData) {
+      return res.status(404).json('No tag found by id');
+    }
+    return res.status(200).json(singleTagData);
+  } catch (err) {
+    return res.status(500).json(err);
+  };
 });
 
 router.post('/', (req, res) => {
