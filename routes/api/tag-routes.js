542f8536b4342c69893adda5a4397eb937ee7d65
Chesney Julian <chesney.julian724@gmail.com>
2023-07-23 14:01:19 -0400

    add route to update tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
+const { update } = require('../../models/Product');
 
 // The `/api/tags` endpoint
 
@@ -36,12 +37,26 @@ router.post('/', async (req, res) => {
     const newTagData = await Tag.create(req.body);
     return res.status(200).json(newTagData);
   } catch (err) {
-  return res.status(500).json(err);
+    return res.status(500).json(err);
   };
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!updateTagData) {
+      return res.status(500).json({message: 'No tag found to update'});
+    } else {
+      return res.status(200).json(updateTagData);
+    };
+  } catch (err) {
+    return res.status(500).json(err);
+  };
 });
 
 router.delete('/:id', (req, res) => {
